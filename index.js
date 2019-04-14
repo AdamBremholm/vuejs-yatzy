73a3520e4a6fb32674e5f1eb2e12e01b17352362
Adam Bremholm <adambremholm@gmail.com>
2019-04-14 14:57:41 +0200

    init

---
+++ index.js
@@ -0,0 +1,37 @@
+const store = new Vuex.store ({
+
+    state : {
+        diceArray : [],
+        dice : {
+            value : null
+        }
+    },
+
+    getters : {
+
+    },
+    
+    mutations : {
+        
+    }
+
+})
+
+const Row = {
+
+
+}
+
+Vue.component('scoreSheet', {
+
+})
+
+new Vue({
+    el: '#app',
+    computed: {
+    },
+    methods : {
+       
+    }
+
+})
