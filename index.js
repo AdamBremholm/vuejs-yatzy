1cb73e4b44838fa886610b9d45931ccef3e21fa9
Adam Bremholm <adambremholm@gmail.com>
2019-04-14 18:53:23 +0200

    roll method and dice updates

--- index.js
+++ index.js
@@ -1,25 +1,6 @@
 const store = new Vuex.Store({
   state: {
-    dices: [
-      {
-        value: 1
-      },
-      {
-        value: 2
-      },
-      {
-        value: 3
-      },
-      {
-        value: 4
-      },
-      {
-        value: 5
-      },
-      {
-        value: 6
-      }
-    ]
+    dice: []
   },
 
   getters: {},
@@ -37,38 +18,87 @@ const Item = {
       `
 };
 
+const Die = {
+  props: ["di"],
+
+  template: `<div>
+      {{di.value}}
+      </div>
+      `
+};
+
+const Actions = {
+  template: `
+          <div >
+              roll, next
+          </div>
+      `
+};
+
 //Ska skriva ut de rullade tärningarna längst ner i appen
 const DiceHolder = {
-    computed: {
-        dices() {
-          return this.$store.state.dices;
-        }
-      },
-    template: `
+  computed: {
+    dice() {
+      return this.$store.state.dice;
+    },
+  },
+  methods : {
+    roll(d) {
+      //Kollar om tärningen är låst och om inte rollar om den, returnerar värdet på tärningen.
+      if (!d.locked) {
+        d.value = Math.floor(Math.random() * 6) + 1;
+      }
+      return d.value;
+    }
+  },
+  template: `
     <div>
-            <div v-for="d, index in dices" v-bind:di="d" v-bind:key="index">{{d.value}}</div>
+            <die v-for="d in dice" v-bind:di="d" :key="d.id">{{roll(d)}}</die>
+  
         </div>
-    `
-}
-
+    `,
+  components: {
+    die: Die
+  }
+};
 
 Vue.component("scoreCard", {
   computed: {
-    dices() {
-      return this.$store.state.dices;
+    dice() {
+      return this.$store.state.dice;
     }
   },
   template: `
         <div>
-            <item-selector v-for="d, index in dices" v-bind:di="d" v-bind:key="index"></item-selector>
+            <item-selector v-for="d in dice" v-bind:di="d" :key="d.id"></item-selector>
         </div>
     `,
   components: {
     "item-selector": Item
-  }
+  } //Kollar om tärninge
 });
 
 const app = new Vue({
-    store,
+  store,
   el: "#app",
+  methods: {
+    //Lägger in 5 tärningar i vuex store
+    initDice() {
+      for (let index = 0; index < 5; index++) {
+        store.state.dice.push({
+          id: index,
+          value: 0,
+          locked: false
+        });
+      }
+    }
+  },
+  //Kör när vuen skapas
+  mounted() {
+    this.initDice();
+  },
+  components: {
+    "dice-holder": DiceHolder,
+    "action-holder": Actions
+  }
 });
