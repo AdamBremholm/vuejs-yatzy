b07b85e4c0bbc4efeb56fe2ce2d09cfa1c457f86
Adam Bremholm <adambremholm@gmail.com>
2019-04-14 16:30:57 +0200

    added some components

--- index.js
+++ index.js
@@ -1,37 +1,74 @@
-const store = new Vuex.store ({
+const store = new Vuex.Store({
+  state: {
+    dices: [
+      {
+        value: 1
+      },
+      {
+        value: 2
+      },
+      {
+        value: 3
+      },
+      {
+        value: 4
+      },
+      {
+        value: 5
+      },
+      {
+        value: 6
+      }
+    ]
+  },
 
-    state : {
-        diceArray : [],
-        dice : {
-            value : null
+  getters: {},
+
+  mutations: {}
+});
+
+//Ska skriva ut varje fält i scorecardet
+const Item = {
+  props: ["di"],
+  template: `
+          <div >
+              {{di.value}}
+          </div>
+      `
+};
+
+//Ska skriva ut de rullade tärningarna längst ner i appen
+const DiceHolder = {
+    computed: {
+        dices() {
+          return this.$store.state.dices;
         }
-    },
-
-    getters : {
-
-    },
-    
-    mutations : {
-        
-    }
-
-})
-
-const Row = {
-
-
+      },
+    template: `
+    <div>
+            <div v-for="d, index in dices" v-bind:di="d" v-bind:key="index">{{d.value}}</div>
+        </div>
+    `
 }
 
-Vue.component('scoreSheet', {
 
-})
-
-new Vue({
-    el: '#app',
-    computed: {
-    },
-    methods : {
-       
+Vue.component("scoreCard", {
+  computed: {
+    dices() {
+      return this.$store.state.dices;
     }
+  },
+  template: `
+        <div>
+            <item-selector v-for="d, index in dices" v-bind:di="d" v-bind:key="index"></item-selector>
+        </div>
+    `,
+  components: {
+    "item-selector": Item
+  }
+});
 
-})
+const app = new Vue({
+    store,
+  el: "#app",
+});
