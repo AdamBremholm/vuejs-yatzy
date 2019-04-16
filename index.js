b7c5c22db1b045fde65394d8590ed8d73492c1b7
Adam Bremholm <adambremholm@gmail.com>
2019-04-16 11:14:19 +0200

    scorecard design complete

--- index.js
+++ index.js
@@ -1,42 +1,7 @@
 const store = new Vuex.Store({
   state: {
     dice: [],
-    scoreCard: [
-      { id: 1, field: "ettor", value: 0 },
-      {
-        id: 2,
-        field: "tvåor",
-        value: 0
-      },
-      {
-        id: 3,
-        field: "treor",
-        value: 0
-      },
-      {
-        id: 4,
-        field: "fyror",
-        value: 0
-      },
-      {
-        id: 5,
-        field: "femmor",
-        value: 0
-      },
-      { id: 6, field: "sexor", value: 0 },
-      { id: 7, field: "bonus", value: 0 },
-      { id: 8, field: "summa", value: 0 },
-      { id: 9, field: "par", value: 0 },
-      { id: 10, field: "två-par", value: 0 },
-      { id: 11, field: "tretal", value: 0 },
-      { id: 12, field: "fyrtal", value: 0 },
-      { id: 13, field: "liten stege", value: 0 },
-      { id: 14, field: "stor stege", value: 0 },
-      { id: 15, field: "kåk", value: 0 },
-      { id: 16, field: "chans", value: 0 },
-      { id: 17, field: "yatzy", value: 0 },
-      { id: 18, field: "totalt", value: 0 }
-    ]
+    scoreCard: []
   },
 
   getters: {},
@@ -44,21 +9,6 @@ const store = new Vuex.Store({
   mutations: {}
 });
 
-//Ska skriva ut varje fält i scorecardet
-const Item = {
-  props: ["it"],
-  computed: {
-    classObject: function() {
-      return "rw " + "rw" + this.it.id;
-    }
-  },
-  template: `
-         <div v-bind:class="classObject">
-          <div class="fi">{{it.field}}</div>
-          <div class="vl">{{it.value}}</div>
-          </div>
-      `
-};
 // Skriver ut varje tärning i tärningsfältet, ska även hålla design för tärningarna
 const Die = {
   props: ["di"],
@@ -103,6 +53,22 @@ const DiceHolder = {
   }
 };
 
+//Ska skriva ut varje fält i scorecardet
+const Item = {
+  props: ["it"],
+  computed: {
+    classObject: function() {
+      return "rw " + "rw" + this.it.id;
+    }
+  },
+  template: `
+         <div v-bind:class="classObject">
+          <div class="fi">{{it.field}}</div>
+          <div class="vl">{{it.value}}</div>
+          </div>
+      `
+};
+
 Vue.component("scoreCard", {
   computed: {
     dice() {
@@ -114,8 +80,10 @@ Vue.component("scoreCard", {
   },
   template: `
         <div class="score-card">
-        <div class="cbhd">Combo</div>
-        <div class="vlhd">Value</div>
+        <div class="cbvl cb1">Combo</div>
+        <div class="cbvl vl1">Value</div>
+        <div class="cbvl cb2">Combo</div>
+        <div class="cbvl vl2">Value</div>
         <item-selector v-for="i, index in scoreCard" v-bind:it="i" :key="index"></item-selector>
         </div>    
     `,
@@ -160,15 +128,19 @@ const app = new Vue({
         "total"
       ];
       for (let index = 0; index < fieldArray.length; index++) {
-        let indexPlusOne = index+1;
-        store.state.scoreCard.push({id: indexPlusOne, field : fieldArray[index], value : 0})
+        let indexPlusOne = index + 1;
+        store.state.scoreCard.push({
+          id: indexPlusOne,
+          field: fieldArray[index],
+          value: 0
+        });
       }
-      
     }
   },
   //Kör när vuen skapas
   mounted() {
     this.initDice();
+    this.initScoreCard();
   },
   components: {
     "dice-holder": DiceHolder,
